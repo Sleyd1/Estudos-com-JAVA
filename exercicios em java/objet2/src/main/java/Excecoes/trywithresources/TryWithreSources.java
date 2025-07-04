32e4ef94aa958d7c6d765ae5186a886071c09bd2
Sleyd1 <153521084+Sleyd1@users.noreply.github.com>
2025-07-03 21:53:29 -0300

    execeções com java

--- exercicios em java/objet2/src/main/java/Excecoes/trywithresources/TryWithreSources.java
+++ exercicios em java/objet2/src/main/java/Excecoes/trywithresources/TryWithreSources.java
@@ -1,23 +1,48 @@
 package Excecoes.trywithresources;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TryWithreSources {
     public static void main(String[] args) {
 
 
+        Scanner sc = new Scanner(System.in);
+
+
+        String s1 = sc.next();
+        int x = sc.nextInt();
+
+        String s2 = sc.next();
+        int x2 = sc.nextInt();
+
+        String s3 = sc.next();
+        int x3 = sc.nextInt();
+
+        System.out.println("================================");
+
+        System.out.printf("%-15s %03d\n", s1, x);
+        System.out.printf("%-15s %03d\n", s2, x2);
+        System.out.printf("%-15s %03d\n", s3, x3);
+
+
+        System.out.println("================================");
+
+
     }
 
-    public  static void lerArquivo(){
+
+    public static void lerArquivo() throws IOException {
         //try com recurso
-            try(Reader reader = new BufferedReader(new FileReader("teste.txt"))){
+        try (Reader reader = new BufferedReader(new FileReader("teste.txt"))) {
 
 
-            } //try com recursos pode ser usado sem o catch
+        } //try com recursos pode ser usado sem o catch
+
+
+
     }
-
 
 }
+
